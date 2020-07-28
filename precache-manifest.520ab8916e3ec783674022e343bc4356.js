06b0a69ef4ef858a9f9fcd4c864887c9011c9d05
Marcos Nicolás Benitez Lisoski <64109161+marcosnbl@users.noreply.github.com>
2020-07-28 07:28:06 -0300

    Updates

---
+++ precache-manifest.520ab8916e3ec783674022e343bc4356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96ceea93f441afe070addbad830478e",
+    "url": "/calculadora-iphone/index.html"
+  },
+  {
+    "revision": "0e1d73f02181faabd17f",
+    "url": "/calculadora-iphone/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "9146213d98a9b85909f1",
+    "url": "/calculadora-iphone/static/css/main.2eb95866.chunk.css"
+  },
+  {
+    "revision": "0e1d73f02181faabd17f",
+    "url": "/calculadora-iphone/static/js/2.0abc90d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora-iphone/static/js/2.0abc90d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9146213d98a9b85909f1",
+    "url": "/calculadora-iphone/static/js/main.61ad62ee.chunk.js"
+  },
+  {
+    "revision": "c255653b21e647665cd9",
+    "url": "/calculadora-iphone/static/js/runtime-main.ab3ae05d.js"
+  }
+]);
